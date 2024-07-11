af6df7516d46366c83e7fde3afc8da857719ff94
Daniel <dan.cherkasky@gmail.com>
2024-07-11 11:14:58 +0300

    final comit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,13 @@ import React from "react";
 import CustomButton from "./CustomButton";
 import Image from "next/image";
 const Hero = () => {
-  const handleScroll = () => {};
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
