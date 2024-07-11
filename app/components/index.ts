3ba7c4b8b4687b0b883e455d5d12501920b34232
Daniel <dan.cherkasky@gmail.com>
2024-07-11 11:08:29 +0300

    initial push

---
+++ app/components/index.ts
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+export { Hero, CustomButton, NavBar, Footer, SearchBar, CustomFilter, SearchManufacturer, CarCard, CarDetails, ShowMore };
+
